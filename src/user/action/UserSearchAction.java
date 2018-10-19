944cf5b85090a0c118a8caa473fd7c1d98c9c3f6
hackone <forrest_gump_@naver.com>
2018-10-19 21:49:42 +0900

    숙제해야댐

---
+++ src/user/action/UserSearchAction.java
@@ -0,0 +1,28 @@
+package user.action;
+
+public class UserSearchAction implements UserAction {
+
+	@Override
+	public void execute() {
+		/*
+		1. 이름 검색
+		2. 아이디 검색
+		번호 입력 : 1
+		
+		찾고자 하는 이름 입력 : 홍
+		홍길동	aaa	111
+		홍당무	bbb	111
+		----------------------
+		
+		찾고자 하는 아이디 입력 : h
+		hong 
+		ddochi
+		
+		* userMapper.xml에는 id = userSearch 인 것 하나만 만들기
+		* 
+		 */
+		
+		
+	}
+
+}
